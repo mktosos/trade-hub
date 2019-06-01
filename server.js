5735bcd4e763f698311dc83a39e89fa61b2a6eea
mktosos <taniguchidesigns@hotmail.com>
2019-06-01 13:37:12 -0700

    aap.use changed back in server.js

--- server.js
+++ server.js
@@ -10,12 +10,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Add routes, both API and view
-app.use(routes);
+
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));  
 }
+
+app.use(routes);
 
 
 
